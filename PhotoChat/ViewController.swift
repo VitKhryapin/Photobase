f1dd1644a6bcdbaf81dede1298d007afbbcee2b6
Vitaly Khryapin <vitkhryapin@gmail.com>
2022-03-25 10:51:48 +0300

    Initial Commit

---
+++ PhotoChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoChat
+//
+//  Created by Vitaly Khryapin on 25.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
